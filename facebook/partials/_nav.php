7825c868b552066330bfac1081092201e39e095c
Vivek Sharma <viveksharma40649@gmail.com>
2026-01-05 14:06:03 +0530

    Update _nav.php

--- facebook/partials/_nav.php
+++ facebook/partials/_nav.php
@@ -9,7 +9,7 @@
 
 echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand mb-2" href="/facebook/index.php">
+    <a class="navbar-brand mb-2" href="/index.php">
       <img src="assets/image.png" alt="Fb-logo" width="37" height="35">
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
@@ -20,24 +20,24 @@ echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class=" btn btn-outline-primary me-3 mb-2" aria-current="page" href="/facebook/index.php">Home</a>
+          <a class=" btn btn-outline-primary me-3 mb-2" aria-current="page" href="/index.php">Home</a>
         </li>';
 
 if ($loggedin) {
     echo '<li class="nav-item">
-            <a class="btn btn-outline-primary me-3 mb-2" href="/facebook/login.php">Login</a>
+            <a class="btn btn-outline-primary me-3 mb-2" href="/login.php">Login</a>
           </li>
           <li class="nav-item">
-            <a class="btn btn-outline-primary me-3 mb-2" href="/facebook/signup.php">Sign Up</a>
+            <a class="btn btn-outline-primary me-3 mb-2" href="/signup.php">Sign Up</a>
           </li>';
 }
 
 if (!$loggedin) {
     echo '<li class="nav-item">
-            <a class="btn btn-outline-primary me-3 mb-2" href="/facebook/myprofile.php">My Profile</a>
+            <a class="btn btn-outline-primary me-3 mb-2" href="/myprofile.php">My Profile</a>
           </li>
           <li class="nav-item">
-            <a class="btn btn-outline-primary mb-2" href="/facebook/messenger.php">Messenger</a>
+            <a class="btn btn-outline-primary mb-2" href="//messenger.php">Messenger</a>
           </li>';
 }
 
@@ -52,7 +52,7 @@ echo '<form class="d-flex me-2" role="search">
       </form>';
 
 if (!$loggedin) {
-    echo '<a class="btn btn-outline-danger" href="/facebook/logout.php">Log Out</a>';
+    echo '<a class="btn btn-outline-danger" href="/logout.php">Log Out</a>';
 }
 
 echo '</div>'; 
@@ -61,3 +61,4 @@ echo '</div>
   </div>
 </nav>';
 ?>
+
